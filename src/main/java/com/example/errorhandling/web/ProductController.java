7170afefd716d933165e852c0bca67c63f0c5b02
dimitar1997 <75738859+dimitar1997@users.noreply.github.com>
2021-11-28 15:33:01 +0200

    Some exercises with errors.

--- src/main/java/com/example/errorhandling/web/ProductController.java
+++ src/main/java/com/example/errorhandling/web/ProductController.java
@@ -1,11 +1,9 @@
 package com.example.errorhandling.web;
 
-import org.springframework.http.HttpStatus;
+
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class ProductController {
